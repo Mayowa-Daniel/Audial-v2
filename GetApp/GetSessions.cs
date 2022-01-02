ebce4b65f010d00ec01938b5f1a9f1042ffc2c59
unknown <mickeyiskingq@gmail.com>
2022-01-02 16:56:38 -0600

    C# part should be completely done

--- GetApp/GetSessions.cs
+++ GetApp/GetSessions.cs
@@ -5,6 +5,7 @@ using NAudio.CoreAudioApi;
 using System.Diagnostics;
 using NAudio.CoreAudioApi.Interfaces;
 using System.Media;
+using System.IO.Ports;
 
 
 namespace GetSession
@@ -17,7 +18,7 @@ namespace GetSession
         /// <summary>
         /// Individual program playing audio 
         /// </summary>
-        private readonly AudioSessionControl session;
+        private AudioSessionControl session;
         Process process;
         string processnamae;
 
@@ -30,100 +31,60 @@ namespace GetSession
         //Collection of all audio Devices
         //private MMDeviceCollection allDevices;
 
-       
 
-        int program;
 
 
         public GetSessions()
         {
-            //Get all Active Audio Renderers(aka speakers,headphones etc) 
-            //allDevices = deviceEnumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
-
             //Get current device being used and all its sessions
             device = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
             Console.Write(device.FriendlyName + "\n");
 
             allcurrdevicesession = device.AudioSessionManager.Sessions;
+        }
 
-            session = allcurrdevicesession[0];
+        public void volumechange(float sys)
+        {
+            //float volume = (float)(Math.Round(session.SimpleAudioVolume.Volume) * 100);
 
-            process = Process.GetProcessById((int)session.GetProcessID);
+            // Console.WriteLine("volume = " + volume);
+            //change Device Audio
+            Console.WriteLine(device.AudioEndpointVolume.MasterVolumeLevelScalar);
 
-            if (session.IsSystemSoundsSession)
-             {
-                Console.WriteLine("System Sounds");
-                processnamae = "System Sounds";
-             }
-            else
-            {
-                Console.WriteLine(process.ProcessName);
-                processnamae = process.ProcessName;
-            }
+            //volume = (float)Convert.ToDouble(Console.ReadLine());
 
+            //absolute/master volume
+            device.AudioEndpointVolume.MasterVolumeLevelScalar = sys / 100.0f;
 
         }
 
-
-        public void volumechange()
+        public int noofapps()
         {
-            float volume = (float)(Math.Round(session.SimpleAudioVolume.Volume)*100);
+            return allcurrdevicesession.Count;
+        }
 
-            Console.WriteLine("volume = " + volume);
-            Console.WriteLine(device.AudioEndpointVolume.MasterVolumeLevelScalar);
-            Console.WriteLine("Process Name:" + processnamae);
-            Console.WriteLine("Change Volume");
-            volume = (float)Convert.ToDouble(Console.ReadLine());
+        public void volumechangeapp(sbyte appo, float app1)
+        {
+            session = allcurrdevicesession[appo];
+            process = Process.GetProcessById((int)session.GetProcessID);
 
-            device.AudioEndpointVolume.MasterVolumeLevelScalar = volume / 100.0f;
+            if (session.IsSystemSoundsSession)
+            {
+                Console.WriteLine(appo + ". System Sounds");
+                processnamae = "System Sounds";
+            }
+            else
+            {
+                Console.WriteLine(appo + ". " + process.ProcessName);
+                processnamae = process.ProcessName;
+            }
 
-            volume = volume / device.AudioEndpointVolume.MasterVolumeLevelScalar;
-            session.SimpleAudioVolume.Volume = 0.5f;
-            
-
-            /*
-            float volume = session.SimpleAudioVolume.Volume;
-            Console.WriteLine(volume);
-            Process.GetProcessById;
-            Console.WriteLine(device.FriendlyName);
-            float volume = session.SimpleAudioVolume.Volume;
-            var volumo = (int)(Math.Round(device.AudioEndpointVolume.MasterVolumeLevelScalar*100));
-            Console.WriteLine(volumo);*/
-
+            //Change session(individual program) volume
+            //will obviously be a percentage of device volume i.e 0.5 = 50% of device volume
+            session.SimpleAudioVolume.Volume = app1;
 
         }
     }
 
+
 }
-
-
-
-/*
-            foreach (MMDevice speaker in allDevices)
-            {
-                Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                Console.WriteLine(speaker.FriendlyName);
-                Console.WriteLine(speaker.State);
-                //Console.WriteLine(speaker.DeviceFriendlyName);
-                //if(speaker.State == DeviceState.Active)
-                alldevicesession = speaker.AudioSessionManager.Sessions;
-                for (int i = 0; i < alldevicesession.Count; i++)
-                {
-                    
-                    session = alldevicesession[i];
-                    if (!session.IsSystemSoundsSession)
-                    {
-                        Process process = Process.GetProcessById((int)session.GetProcessID);
-                        Console.WriteLine(process.ProcessName);
-                        //Console.WriteLine(process.ProcessName+" "+ process.MainWindowTitle);
-                        //Console.WriteLine(i + session.DisplayName);
-                        //Console.WriteLine(session.State);
-
-                    }
-
-
-                }
-
-            }
-
-*/
